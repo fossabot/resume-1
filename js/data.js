18a5c57a44e9432ade24b91ff3876332056f5dad
wuEva <neefoxmo@gmail.com>
2018-03-25 13:43:51 +0800

    更新相关数据源状态

--- js/data.js
+++ js/data.js
@@ -15,7 +15,7 @@ function getStackData(){
         },  
         "software":
         {
-            "skilled":["PS","AI","FLASH","ATOM","VSCODE","VS2010","Hbuilder","Chrome","X-MIND","Ngrok","Axure","eclipse","Tortoise"],
+            "skilled":["PS","AI","FLASH","ATOM","VSCODE","iceworks","VS2010","Hbuilder","Chrome","X-MIND","Ngrok","Axure","eclipse","Tortoise"],
             "familiar":["PR","AE","axure","Git Bash","sublime","shadowsocks"]
         }                          
     };
@@ -27,9 +27,9 @@ function getExperData(){
         "school":
           {
             "graTime": "2017年",
-            "name": "武汉华夏理工学院",
-            "expr":"混过社团，举办过比赛，当过代班。",
-            "record":"一次二等助学金、两次国家励志奖学金、三次系级优秀奖励、两次院级优秀奖励。",
+            "name": "华夏理工学院(原武汉理工大学华夏学院)",
+            "expr":"混过社团，举办过比赛，当过代班，参加过小跳。",
+            "record":"一次二等助学金、两次国家励志奖学金、三次系级优秀奖励、两次院级优秀奖励，省级三等奖。",
             "ranking":"一般专业排名top1"
           }
         ,
@@ -47,96 +47,60 @@ function getExperData(){
                 "duty":"前期主要是使用js和t-sql在公司研发平台进行二次表单开发，涉及到数据库字段设计和多表操作。<br/>后期进入互联网项目，主要是利用c#,html(5),css(3),js，bootstrap,ligerui,等等对项目节目进行规划和设计，需要保证项目在多浏览器和不同设备的兼容性。<br/>目前已经有三个上线项目经验，在项目实战中主要写过三个小轮子，支持中英文输入的虚拟键盘，弹窗插件和流程插件。<br/>现在主要独立负责公司webgis项目组"
             }            
           ],
-        "evaluate":"兴趣是最好的老师，喜欢前端所思所想即可写成所得，个人自学能力较强，责任感和韧性比较强，毕竟是在项目上线前现场临时改过代码的人。<br/>喜欢折腾和技术分享，转化设计图效率比较高，工作之余喜欢按照规划补充自己的技术栈，也比较喜欢学习的状态。<br/>目前用过前端三大框架写过webapp，也写过微信小程序，nodejs也玩过一段时间。<br/>软件上方面版本管理的svn和git使用比较熟练，后台主要是对.net比较熟悉，换了多个编辑器，目前比较喜欢用的是vscode。<br/>对自己的要求是今天事今日毕，工作上喜欢接受挑战，目前有几个自己写的几个小轮子，最近刚改了一个hexo主题包，hexo-theme-melon,自己扩展了一些功能，主要样式也改了一些，后期如果有更好样式或者功能的建议，我会继续修改这个主题，欢迎clone提意见。"
+        "evaluate":"兴趣是最好的老师，喜欢前端所思所想即可写成所得，个人自学能力较强，责任感和韧性比较强，毕竟是在项目上线前现场临时改过代码的人。<br/>喜欢折腾和技术分享，转化设计图效率比较高，工作之余喜欢按照规划补充自己的技术栈，也比较喜欢学习的状态。<br/>目前用过前端三大框架写过webapp，也写过微信小程序，nodejs也玩过一段时间。<br/>软件上方面版本管理的svn和git使用比较熟练，后台主要是对.net比较熟悉，换了多个编辑器，目前比较喜欢用的是vscode。项目了，除去一些网页模板和技术学习记录，有几个是自己写的几个小小轮子，虚拟键盘支持中英文书写的轮子，支持各种参数设计的自动化流程轮子，一个支持各种细节参数设置的模态窗轮子，还有就是三个vue的开箱即用的模板项目。<br/>在 node 方面，改过一个基于 ejs 的 hexo 博客主题，自己扩展了一些功能，主要样式也改了一些，后期如果有更好样式或者功能的建议，我会继续修改这个主题，欢迎clone提意见。"
         };
     return  dataE;   
 }
 
 //获取作品集数据
 function getWorksData(){
-    var dataW =[
-            {
-                "name":"hexo-theme-melon",
-                "describe":"在现有主题基础上，个人定制一定样式，支持gitment的hexo主题，修改之后的主题适合girl",
-                "src":"img/web/melon.jpg",
-                "tags":["hexo","sass","gulp","gitment","npm","ejs","git","webpack"],
-                "url":"https://github.com/mowatermelon/hexo-theme-melon.git"
-            },
-            {
-                "name":"vueNew",
-                "describe":"基于vue全家桶写的一个新闻类的单页面，主要有新闻，美图，美文和设置等页面",
-                "src":"img/apk/vueNew.jpg",
-                "tags":["vue","vue-router","vue-resource","node","npm","webpack","mdui"],
-                "url":"https://github.com/mowatermelon/vueNew.git"
-            },            
-            {
-                "name":"modal.js",
-                "describe":"在现有框架基础上高度集成一个模态窗插件，支持多个参数的定制，放上使用文档。",
-                "src":"img/plug/showmodal.jpg",
-                "tags":["bootstrap","jQuery","css","gitbook"],
-                "url":"https://github.com/mowatermelon/demoArray/blob/master/showmodal.md"
-            },
+        var dataW =[
             {
                 "name":"visualKeyboard",
-                "describe":"自己写的支持中文和英文输入的虚拟键盘，主要业务场景是PC查询机，参考搜狗默认皮肤。",
-                "src":"img/plug/keyboard.jpg",
+                "describe":"自己写的支持中文和英文输入的虚拟键盘，主要业务场景是PC查询机，中文输入法有搜狗默认皮肤、西瓜皮肤和三个coding洋葱猴皮肤。",
                 "tags":["css","jquery","simpleInputMethod","designByMe"],
                 "url":"https://github.com/mowatermelon/visualKeyboard.git"
-            },                       
+            }, 
             {
-                "name":"flow.js",
-                "describe":"自动化流程插件，样式基于bootstrap和内容基于配置文档的流程页面跳转插件",
-                "src":"img/plug/flow.jpg",
-                "tags":["bootstrap","js","自动化"],
-                "url":"https://github.com/mowatermelon/flowdemo.git"
-            },
+                "name":"modal.js",
+                "describe":"这是一个基于bootstrap二次开发的dialog插件，支持多个参数的定制，放上使用文档，使用本插件之前需要引用jquery.js、bootstrap.css和bootstrap.js。",
+                "tags":["bootstrap","jQuery","css","gitbook"],
+                "url":"https://github.com/mowatermelon/demoArray/blob/master/showmodal.md"
+            }, 
             {
-                "name":"bingoGame",
-                "describe":"这个是仿造美国bingo游戏写的一个小的demo，通过判断一个随机的匹配结果之和来计算输赢",
-                "src":"img/web/bingoGame.png",
-                "tags":["bingo","js","css","designByMe"],
-                "url":"https://github.com/mowatermelon/bingoCard.git"
-            },           
+                "name":"hexo-theme-melon",
+                "describe":"在hexo-theme-yilia主题基础上，个人定制一定样式，支持gitment的hexo主题，添加文章内容检索功能，修改之后的主题适合girl",
+                "tags":["hexo","sass","gulp","gitment","npm","ejs","git","webpack"],
+                "url":"https://github.com/mowatermelon/hexo-theme-melon.git"
+            },         
             {
-                "name":"Q & A web",
-                "describe":"自己写的一个商务类型的问答网站模版",
-                "src":"img/web/QAweb.png",
-                "tags":["css","响应式","designByMe"],
-                "url":"https://github.com/mowatermelon/mo.git"
-            },            
+                "name":"all-year-calendar",
+                "describe":"基于jq，amazeui与bootstrap的一个全年考勤日历插件，支持工作日和非工作日状态的修改设置，支持全年日历的切换，并基于项目实际情况优化了界面效果",
+                "tags":["amazeui","jquery","calender","all-year"],
+                "url":"https://github.com/mowatermelon/all-year-calendar.git"
+            }, 
             {
-                "name":".NETSHOP",
-                "describe":"自己写的一个购物商城网站模版",
-                "src":"img/web/Adminlogin.png",
-                "tags":["c#","SqlServer","css","designByMe"],
-                "url":"https://github.com/mowatermelon/.netSHOP.git"
-            },
+                "name":"moTool",
+                "describe":"集合了一些平时写的小工具函数",
+                "tags":["vue","plugins","layui-demo","motool"],
+                "url":"https://github.com/mowatermelon/moTool.git"
+            },                   
             {
-                "name":"leafblog",
-                "describe":"自己写的一个小清新的个人blog模版",
-                "src":"img/web/leafblog.jpg",
-                "tags":["css","jquery","jquery.poptrox","designByMe"],
-                "url":"https://github.com/mowatermelon/blog_leaf.git"
-            },  
+                "name":"vue-webapp",
+                "describe":"基于vue做的webapp模版，基于mdui的webapp，在mdui分支",
+                "tags":["axios","vue","vue-router","webpack","express","mdui","vuex","localstorage"],
+                "url":"https://github.com/mowatermelon/vue-webapp.git"
+            },   
             {
-                "name":"qqGame",
-                "describe":"学生时期写的一个腾讯游戏活动的页面",
-                "src":"img/web/qqGame.png",
-                "tags":["css","qqGame","designByMe"],
-                "url":"https://github.com/mowatermelon/qqcargame2.0.git"
-            },
-            {
-                "name":"design",
-                "describe":"学生时期设计的一些小物料",
-                "src":"img/plug/showmodal.jpg",
-                "tags":["ps","ai","flash","designByMe"],
-                "url":"https://github.com/mowatermelon/picCreations.git"
-            },                                     
+                "name":"vue-admin",
+                "describe":"基于axios，bootstrap，vue-router，webpack和express等等的基础vue后台控制模板，默认有三个颜色主题可切换。",
+                "tags":["axios","vue","vue-router","webpack","vue-admin","express","mo-theme","vuex","localstorage"],
+                "url":"https://github.com/mowatermelon/vue-admin.git"
+            },             
             {
                 "name":"更多作品",
                 "describe":"更多作品可以去我的github仓库中查看",
-                "src":"img/web/qqGame.png",
-                "tags":["bootstrap","vue","angular","es6","node","html2canvas","echart","json","io","localstorage","session","mvvm","OOCSS","bem"],
+                "tags":["bootstrap","vue","angular","electron","ionic","es6","node","python","iceworks","html2canvas","echart","json","io","localstorage","session","mvvm","OOCSS","bem"],
                 "url":"https://github.com/mowatermelon"
             }          
         ];
@@ -157,7 +121,7 @@ function getAboutData(){
             "expr":"两年",
             "job":"前端工程师",
             "workAdd":"服从安排",
-            "salary":"9k+",
+            "salary":"8k+",
             "blog":"https://mowatermelon.github.io/"
         };
     return  dataA;   
